6ce99b36d99d7436ebc981cacb74f2411c1787a9
Francisco Fernandes <franciscofeernandes96@hotmail.com>
2021-04-29 16:45:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useState,useEffect } from "react";
 import './App.css';
+//Components
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+import Footer from './components/Footer';
 
 function App() {
+  //State
+  const [inputText, setInputText] = useState(""); //input
+  const [todos, setTodos] = useState([]); //list
+  const [status, setStatus] = useState('all'); //states dos botoes
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  
+  //Use Effect
+  useEffect(() => {
+     filterHandler();
+  }, [todos,status]);
+
+  //Functions
+  const filterHandler = () => {
+    switch(status) {
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true))
+        break;
+      case 'active':
+        setFilteredTodos(todos.filter(todo => todo.completed === false))
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>todos</header>
+      <main>
+      <section className="container">
+        <Form 
+        inputText={inputText} 
+        todos={todos} 
+        setTodos={setTodos} 
+        setInputText={setInputText}>
+        </Form>
+        <TodoList filteredTodos={filteredTodos} todos={todos} setTodos={setTodos}></TodoList>
+        <Footer setTodos={setTodos} todos={todos} setStatus={setStatus}></Footer>
+        
+      </section>
+      </main>
     </div>
   );
 }
